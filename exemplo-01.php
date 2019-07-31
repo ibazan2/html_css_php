85bf0b069b4004fc5128cf32dcd3463d8c877f9a
italo_anast17 <ibazan2@hotmail.com>
2019-07-31 09:31:31 -0300

    Subindo os arquivos

---
+++ exemplo-01.php
@@ -0,0 +1,11 @@
+<?php
+
+$nome = "HCode - - ";
+
+$soma = 5+10^2;
+
+echo $nome;
+echo $soma;
+var_dump($nome);
+
+?>
---
+++ exemplo-02.php
@@ -0,0 +1,29 @@
+<?php
+
+$anoNascimento = 1984;
+
+$nomeCompleto = "Italo Bazan";
+
+echo $nomeCompleto," nasceu em " , $anoNascimento;
+echo "<br>";
+
+if (isset($nomeCompleto)){
+	echo "entrei";
+	//echo "<br>";
+	echo "<p>";
+}
+unset($nomeCompleto);
+if (isset($nomeCompleto)){
+	echo "entrei";
+}else{
+	//echo "<br>";
+	echo "Não existe Mais";
+	echo "<p> <font size='2000' color='blue'> ok </FONT>";
+	echo "Não existe Mais";
+	echo "<br>";
+	echo "Não existe Mais";
+
+}
+
+
+?>
